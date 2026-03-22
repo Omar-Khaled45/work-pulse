bf85970fe4c83fc230bf90fb4de736f517514571
Omar Khaled <omar.khaled4550@gmail.com>
2026-03-22 16:39:41 +0200

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-green-300">
+      <div className="h-52 w-52 cursor-pointer rounded-xl border border-black bg-gray-400 shadow-xl transition-all duration-300 hover:-translate-y-2"></div>
+    </div>
+  );
+};
+
+export default App;
